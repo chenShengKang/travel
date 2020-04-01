ecb465a662f495f2ccbd60251c6e69b90d1f21d3
csk <2432742181@qq.com>
2020-04-01 19:30:29 +0800

    product

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//导入 移动端 点击延迟300ms的缺陷
+import fastClick from 'fastclick'
+//配置到 dom 上
+fastClick.attach(document.body)
+
 Vue.config.productionTip = false
 
 new Vue({
