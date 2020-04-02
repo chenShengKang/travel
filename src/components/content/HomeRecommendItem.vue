c9ef672fb89817149d11498a862bb3b38355f900
csk <2432742181@qq.com>
2020-04-02 15:24:58 +0800

    recommend

---
+++ src/components/content/HomeRecommendItem.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="recommend-item" >
+    <div class="recommend-header">热销推荐</div>
+    <div class="recommend-center" v-for="item of recommendItem" :key="item.id">
+      <div class="img">
+        <img :src="item.imgUrl" alt="">
+      </div>
+      <div class="content">
+        <p class="title">{{item.title}}</p>
+        <div class="commoned-wrap">
+          <span class="commend">{{item.num}}评论</span>
+        </div>
+        <div class="price-wrap">
+          <span class="dollor">￥</span>
+          <span class="price">
+            {{item.price}}
+            </span>
+            <span class="othor">起</span>
+          <span class="city">{{item.city}}</span>
+        </div>
+        <p class="desc">{{item.desc}}</p>
+      </div>
+    </div>
+    <div class="more">
+      查看更多
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeRecommendItem",
+  props: {
+    recommendItem: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  filters:{
+    price(value){
+      return '￥'+value
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.recommend-header{
+  padding: 12px 0;
+  font-size: 18px;
+  text-indent: 30px;
+}
+.more{
+  text-align: center;
+  color: #00afc7;
+  border-top: 1px solid #f5f5f5;
+  padding: 5px 0;
+}
+.recommend-center {
+  position: relative;
+  margin: 10px 0;
+  display: flex;
+  .content{
+    flex: 1;
+    
+    padding: 10px;
+    .commoned-wrap{
+      margin-top: 10px;
+    }
+    .price-wrap{
+      display: flex;
+      margin-top: 10px;
+      .city{
+        margin-left: auto;
+      }
+      .dollor{
+        color: #ff8300;
+        vertical-align: bottom;
+      }
+      .price{
+        font-size: 20px;
+        color: #ff8300;
+        vertical-align: middle;
+      }
+      .othor{
+        vertical-align: 10px;
+      }
+    }
+    .title{
+      font-size: 16px;
+    }
+    .desc{
+      margin-top: 10px;
+      color: #ff5555;
+    }
+  }
+  .img{
+    width: 100px;
+    height: 100px;
+    img{
+      width: 100%;
+    }
+  }
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,7 @@
    <header-nav-bar></header-nav-bar>
    <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import HomeSwiper from './child/HomeSwiper.vue'
 import HeaderNavBar from './child/HeaderNavBar.vue'
 import HomeIcons from './child/HomeIcons.vue'
+import HomeRecommend from './child/HomeRecommend.vue'
 export default {
   name:'Home',
   components: {
     HomeSwiper,
     HeaderNavBar,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   }
 }
 </script>
